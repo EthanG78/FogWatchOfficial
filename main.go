186688449e8eff5113c3459c09d1da8303602a96
Ciisco <EthanBB13@gmail.com>
2017-11-03 14:57:34 -0300

    Now serving webpages as Go Templates

--- main.go
+++ main.go
@@ -1,13 +1,37 @@
 package main
 
 import (
+	"html/template"
 	"log"
 	"net/http"
+	"time"
 )
+
+type payload struct {
+	Date string /*
+		Location string
+		Temp     string
+		Humidity string
+		Wind     string*/
+	Status string
+}
+
+func index(w http.ResponseWriter, r *http.Request) {
+	currentTime := time.Now().Local()
+	send := payload{
+		Date:   currentTime.Format("01-02-2006"),
+		Status: "Active",
+	}
+	t, err := template.ParseFiles("static/templates/index.html")
+	if err != nil {
+		log.Fatalf("Error parsing template: %v", err)
+	}
+	t.Execute(w, send)
+}
 
 func main() {
 
-	http.Handle("/", http.FileServer(http.Dir("./static/templates")))
+	http.HandleFunc("/", index)
 	log.Println("Now serving on localhost:8080")
 	log.Fatal(http.ListenAndServe("localhost:8080", nil))
 }
